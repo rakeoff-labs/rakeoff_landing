fe78f93b7469830bba65aecdb8b38bd0c93afb20
jesssekeogh <jesse.keogh74@gmail.com>
2023-11-15 14:17:44 +0000

    error handling json

--- src/rakeoff_landing_frontend/src/components/tools.js
+++ src/rakeoff_landing_frontend/src/components/tools.js
@@ -4,11 +4,21 @@ export function e8sToIcp(x) {
 }
 
 export const getRakeoffStats = async () => {
-  const response = await fetch(
-    "https://jgvzt-eiaaa-aaaak-ae5kq-cai.raw.icp0.io/v1/rakeoff-stats"
-  ).then((x) => x.json());
-
-  return response.icp_stats
+  try {
+    const response = await fetch(
+      "https://jgvzt-eiaaa-aaaak-ae5kq-cai.raw.icp0.io/v1/rakeoff-stats"
+    );
+    if (!response.ok) {
+      // If the response is not ok (e.g., 404, 500), return an empty object
+      return {};
+    }
+    const data = await response.json();
+    return data.icp_stats || {}; // Return icp_stats, or an empty object if icp_stats is undefined
+  } catch (error) {
+    // In case of a network error or json parsing error, return an empty object
+    console.error("Error fetching rakeoff stats:", error);
+    return {};
+  }
 };
 
 export const getApyEstimate = async () => {
