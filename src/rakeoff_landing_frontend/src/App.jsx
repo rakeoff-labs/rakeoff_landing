7f2ee85c045e6d03880bfb23de8a3c2b312094db
jesssekeogh <jesse.keogh74@gmail.com>
2025-01-15 22:29:44 +0000

    Updating landing to Rakeoff.io known neuron

--- src/rakeoff_landing_frontend/src/App.jsx
+++ src/rakeoff_landing_frontend/src/App.jsx
@@ -1,49 +1,178 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
 import {
-  Banner,
-  MoreOnRakeoff,
-  Footer,
-  Features,
-  Rakeoff,
-  AllSteps,
-} from "./sections";
+  boxBackgroundColor,
+  subFontColor,
+  lightColor,
+  darkColor,
+  lightBorderColor,
+  darkBorderColor,
+} from "./colors";
+import { MoonIcon, SunIcon, CheckCircleIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Spacer,
+  Button,
+  Heading,
+  List,
+  ListItem,
+  ListIcon,
+  Highlight,
+  Image as ChakraImage,
+  useColorMode,
+} from "@chakra-ui/react";
+import logo from "../assets/rakeoff_logo_white.svg";
+import xLogo from "../assets/x_logo.svg";
+import discordLogo from "../assets/discord_logo.svg";
+import githubLogo from "../assets/github_logo.svg";
 
 const App = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Router>
-      <Routes>
-        <Route path="*" element={<AppLayout />} />
-      </Routes>
-    </Router>
+    <>
+      <Flex h="10vh" align="center" justify="center">
+        <Flex w={{ base: "90%", md: "80%" }} align="center">
+          <ChakraImage alt="Rakeoff logo" src={logo} h={35} />
+          <Spacer />
+          <Button
+            onClick={toggleColorMode}
+            rounded="full"
+            boxShadow="base"
+            aria-label="Change color mode"
+          >
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          </Button>
+        </Flex>
+      </Flex>
+      <Flex align="center" justify="center" h="80vh">
+        <Box
+          bgGradient={`linear(to-bl, ${boxBackgroundColor}, purple.500, #6229a8)`}
+          h="90%"
+          w={{ base: "90%", md: "80%" }}
+          borderRadius="lg"
+        >
+          <Flex align="center" justify="center" h="100%">
+            <Box>
+              <Heading
+                lineHeight="tall"
+                align="center"
+                color={colorMode === "light" ? lightColor : darkColor}
+                fontFamily="'Roboto Mono', monospace"
+                fontSize={{ base: "lg", md: "3xl" }}
+                maxW="xl"
+                px={3}
+              >
+                <Highlight
+                  query={["Rakeoff.io"]}
+                  styles={{
+                    px: "1",
+                    py: "1",
+                    color: colorMode === "light" ? lightColor : darkColor,
+                    borderBottom: `double ${
+                      colorMode === "light" ? lightColor : darkColor
+                    } 3px`,
+                  }}
+                >
+                  The Rakeoff.io Known Neuron!
+                </Highlight>
+              </Heading>
+              <Flex
+                align="center"
+                justify="center"
+                direction="column"
+                p={6}
+                gap={6}
+              >
+                <List maxW="md" align="start">
+                  <StyledListItem text="A known neuron on the Network Nervous System that always votes." />
+                  <StyledListItem text="Advocates for an open and permissionless ICP with many voices." />
+                  <StyledListItem text="Currently following Krzysztof Żelazko on all topics." />
+                  <StyledListItem text="Follow the new @NeuronPool X account for updates." />
+                </List>
+              </Flex>
+            </Box>
+          </Flex>
+        </Box>
+      </Flex>
+      <Flex h="10vh" align="center" justify="center">
+        <Flex
+          w="80%"
+          align="center"
+          direction={{ base: "column", md: "row" }}
+          gap={2}
+          mb={6}
+        >
+          <Heading
+            color={subFontColor}
+            fontFamily="'Roboto Mono', monospace"
+            size="sm"
+          >
+            crew@rakeoff.io
+          </Heading>
+          <Spacer />
+          <Flex align="center" gap={3}>
+            <SocialIconLink
+              image={xLogo}
+              alt={"twitter link"}
+              link={"https://x.com/NeuronPool"}
+              xLogo
+            />
+            <SocialIconLink
+              image={discordLogo}
+              alt={"discord link"}
+              link={"https://discord.gg/5jRHUYnsrM"}
+            />
+            <SocialIconLink
+              image={githubLogo}
+              alt={"github link"}
+              link={"https://github.com/rakeoff-labs"}
+            />
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
   );
 };
 
 export default App;
 
-const AppLayout = () => {
+const StyledListItem = ({ text }) => {
+  const { colorMode } = useColorMode();
   return (
-    <Box>
-      <BackgroundSection>
-        <Banner />
-        <Features />
-        <Rakeoff />
-        <AllSteps />
-        <MoreOnRakeoff />
-        <Footer />
-      </BackgroundSection>
-    </Box>
+    <ListItem
+      mb={3}
+      fontWeight={600}
+      fontSize={{ base: "sm", md: "lg" }}
+      color={colorMode === "light" ? lightColor : darkColor}
+      fontFamily="'Roboto Mono', monospace"
+    >
+      <ListIcon
+        as={CheckCircleIcon}
+        color={colorMode === "light" ? lightColor : darkColor}
+      />
+      {text}
+    </ListItem>
   );
 };
-// checkered pattern
-const BackgroundSection = ({ children }) => {
+
+const SocialIconLink = ({ image, alt, link, xLogo }) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Box
-      bg="linear-gradient(#292e40 2px, transparent 1px), linear-gradient(to right, #292e40 2px, #1b2030 1px)"
-      backgroundSize=" 120px 120px"
-    >
-      {children}
-    </Box>
+    <a href={link} target="_blank" rel="noreferrer">
+      <Box
+        rounded="full"
+        border={
+          colorMode === "light"
+            ? `solid ${lightBorderColor} 1px`
+            : `solid ${darkBorderColor} 1px`
+        }
+        p={2}
+        boxShadow="base"
+        _hover={{ opacity: "0.8", cursor: "pointer" }}
+      >
+        <ChakraImage h={"22px"} p={xLogo ? "3px" : 0} src={image} alt={alt} />
+      </Box>
+    </a>
   );
 };
--- src/rakeoff_landing_frontend/src/colors.js
+++ src/rakeoff_landing_frontend/src/colors.js
@@ -1,7 +1,7 @@
 export const mainBackgroundColor = "#1b2030";
 export const boxBackgroundColor = "#292e40";
-export const RakeoffRed = "#e53123";
-export const boxBorderColor = "solid #363b4c 2px";
-export const boxFontColor = "#a5a8b6";
-export const RakeoffGrey = "#e2e2e2";
-export const RakeoffPurpleHue = "#9d5bc2";
+export const subFontColor = "#a9acb1";
+export const lightColor = "#fefbf1";
+export const darkColor = "#1d252a";
+export const lightBorderColor = "#e2e8f0";
+export const darkBorderColor = "#4a5157";
